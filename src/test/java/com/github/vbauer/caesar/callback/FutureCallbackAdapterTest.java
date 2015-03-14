98ed0dbb7a2fd1dac59941fd3a607b12eaeb499e
Vladislav Bauer <bauer.vlad@gmail.com>
2015-03-15 05:22:54 +0600

    Add unit tests for FutureCallbackAdapter

---
+++ src/test/java/com/github/vbauer/caesar/callback/FutureCallbackAdapterTest.java
@@ -0,0 +1,41 @@
+package com.github.vbauer.caesar.callback;
+
+import com.github.vbauer.caesar.basic.BasicTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author Vladislav Bauer
+ */
+
+public class FutureCallbackAdapterTest extends BasicTest {
+
+    @Test
+    public void testCallback() {
+        final AtomicBoolean onSuccess = new AtomicBoolean(false);
+        final AtomicBoolean onFailure = new AtomicBoolean(false);
+
+        final FutureCallbackAdapter<Object> callback = new FutureCallbackAdapter<Object>() {
+            @Override
+            public void onSuccess(final Object result) {
+                super.onSuccess(result);
+                onSuccess.set(true);
+            }
+
+            @Override
+            public void onFailure(final Throwable caught) {
+                super.onFailure(caught);
+                onFailure.set(true);
+            }
+        };
+
+        callback.onSuccess(null);
+        callback.onFailure(null);
+
+        Assert.assertTrue(onSuccess.get());
+        Assert.assertTrue(onFailure.get());
+    }
+
+}
